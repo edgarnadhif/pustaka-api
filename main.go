850a1f2dacbda27982c9ee8f6987ca0054399732
bagashiz <work.bitsy@simplelogin.com>
2022-12-26 17:44:03 +0700

    implement path variables and query strings

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 
 	router.GET("/", rootHandler)
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 
 	router.Run(":8888")
 }
@@ -24,7 +26,26 @@ func rootHandler(ctx *gin.Context) {
 
 func helloHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
-		"name": "Bagas Hizbullah",
-		"bio":  "Information Systems student",
+		"title": "hello world!",
+	})
+}
+
+func booksHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	title := ctx.Param("title")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(ctx *gin.Context) {
+	title := ctx.Query("title")
+	price := ctx.Query("price")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
 	})
 }
